6f8009004aeedaf3f0100758eedc4373f559d4b9
Matthias <matthiastutor@outlook.de>
2019-10-23 20:10:11 +0200

    REFACTOR #9 polygon reconstruction test and lower minimalStep bound for descend methods

--- venv/polygonReconstructTest.py
+++ venv/polygonReconstructTest.py
@@ -18,19 +18,20 @@ while repeats < 50:
 
     result = pR.polygonReconstructCentered( cD_test , 0.0001 , 0.5 , 0.9 , 0.5 , 0.5 , eps_test)
 
-    if M.dist( cV.gamma( cD_test , result ) , P_test[0]) > 0.01:
-        sigma_result = cV.sigma(result , cD_test )
+    sigma_result = cV.sigma(result, cD_test)
 
-        if( sigma_result < eps_test):
-            noFail += 1
+    if (sigma_result < eps_test):
+        noFail += 1
+    else:
+        if (sigma_result < eps_test * 100):
+            fail_soft += 1
         else:
-            if( sigma_result < eps_test * 100 ):
-                fail_soft += 1
+            if (sigma_result < eps_test * 100 * 100):
+                fail_medium += 1
             else:
-                if (sigma_result < eps_test * 100 * 100):
-                    fail_medium += 1
-                else:
-                    fail_hard += 1
+                fail_hard += 1
+
+    if M.dist( cV.gamma( cD_test , result ) , P_test[0]) > 0.01:
 
 
         if sigma_result < eps_test:
@@ -50,8 +51,8 @@ while repeats < 50:
 
     repeats += 1
     print(repeats)
+    print([noFail, fail_soft, fail_medium, fail_hard])
 
-print( [ noFail , fail_soft , fail_medium , fail_hard ] )
 
 # results where everything worked
 P_1 = [[3.50838286589081, -1.5107444243803627], [2.325398721911787, -0.21029997610538254], [-1.485558813118278, 3.5717000639909195], [-2.8198717746014843, 2.0978600608178417], [-1.5283510000828333, -3.9485157243230167]]
